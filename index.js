3599096412ec3e2b305c451a6a5c1486a2c11d54
Klenton Stone <sto10026@byui.edu>
2018-07-12 10:22:14 -0600

    adding files

--- index.js
+++ index.js
@@ -1,27 +1,85 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+var bodyParser = require('body-parser');
+
+// Database connection 
+const { Pool } = require('pg');
+const connectionString = process.env.DATABASE_URL || "postgres://klentonstone:password@localhost:5432";
+const pool  = new Pool({connectionString: connectionString});
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json())   // support json encoded bodies
+  .use(express.static(__dirname + '/public'))
   .use(express.json())
   .use(express.urlencoded({extended: true}))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/stream', handleStream)
+  // .get('/site', handleSite)
+  // .get('/journal', handleJournal)
   .post('/signup', postSignUp)
   .post('/login', postLogIn)
-  .post('/search', searchData)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function handleStream(req, res) {
+	var stream = req.query.stream;
+	console.log("Looking for stream with name: " + stream);
+
+	getStreamFromDB(stream, function(err, result) {
+		if (err){
+			console.log("ERROR: " + err);
+		}else {
+			res.json(result);
+		}
+	});
+}
+
+// function handleSite(req, res) {
+	
+// }
+
+// function handleJournal(req, res) {
+	
+//}
+
+
+function getStreamFromDB(stream, callback) {
+	pool.query('SELECT * FROM stream WHERE name = $1::text', [stream], function(err, res) {
+		if (err) {
+			throw err;
+		}else {
+			// We got the reuslt for the db..
+			console.log("Back from DB with: " + res.rows);
+
+			var results = {
+				status: 'success',
+				list: res.rows
+			};
+
+			callback(null, result);
+		}
+	})
+}
+
 
 function postSignUp(req, res) {
 	console.log("Signing up...");
+	var userName = req.body.uName;
+	var passwrd = req.body.psw;
+	var rptpasswrd = req.body.pswrepeat;
+	
+	console.log(userName + ": " + passwrd + " : " + rptpasswrd); 
+
+	// call to check user against Database. 
+	//var sql = "SELECT id, username, password FROM app_user WHERE password=" + passwrd;
+
+	//console.log(sql);
+
+
+	res.redirect('/home.html');
 }
 
 function postLogIn(req, res) {
 	console.log("Logging in...");
+	res.redirect('/home.html');
 }
 
-function searchData(req, res) {
-	console.log("Searching Database...");
-}
---
+++ public/stream.js
@@ -0,0 +1,32 @@
+function streamSearch() {
+	// Log something on the browser console
+	console.log("Searching...");
+
+	var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+           if (xmlhttp.status == 200) {
+               //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+               //console.log("Back from the server with: " + xmlhttp.responseText);
+               updatePage(xmlhttp.responseText);
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+
+    // TODO: I need to get the book name from the form here...
+    var streamName = document.getElementById("stream").value;
+
+    xmlhttp.open("GET", "/stream?stream=" + streamName, true);
+    xmlhttp.send();	
+}
+
+function updatePage(results) {
+  console.log("Updating the div with these results: " + results);
+}
