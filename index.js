ea8d944440d8a2224e613571f4ac70098635873f
Klenton Stone <sto10026@byui.edu>
2018-07-15 09:35:51 -0600

    adding everything new

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 // Database connection 
 const { Pool } = require('pg');
-const connectionString = process.env.DATABASE_URL; //|| "postgres://klentonstone:password@localhost:5432/";
+const connectionString = process.env.DATABASE_URL || "postgres://nodeuser:password@localhost:5432/postgres";
 const pool  = new Pool({connectionString: connectionString});
 
 express()
@@ -14,11 +14,38 @@ express()
   .use(express.urlencoded({extended: true}))
   .get('/', (req, res) => res.render('pages/index'))
   .get('/stream', handleStream)
+  .get('/allStreams', handleAllStreams)
+  .get('/infoOfStream', getInfoOfStream)
   // .get('/site', handleSite)
   // .get('/journal', handleJournal)
   .post('/signup', postSignUp)
   .post('/login', postLogIn)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+function getInfoOfStream(req, res) {
+	var stream = req.query.stream;
+	//console.log("StreamA = " + stream);
+	getStreamInfo(stream, function(err, result) {
+		if (err) {
+			throw err;
+		}else {
+			console.log("returned object");
+			res.json(result);
+		}
+	})
+}
+
+function handleAllStreams(req, res) {
+	//console.log("I GOT HERE")
+ 	getAllStreamsFromDB(function(err, result) {
+ 		if (err) {
+ 			throw err;
+ 		}else {
+ 			res.json(result);
+ 		}
+ 	});
+}
 
 function handleStream(req, res) {
 	var stream = req.query.stream;
@@ -41,9 +68,46 @@ function handleStream(req, res) {
 	
 //}
 
+function getStreamInfo(stream, callback) {
+	console.log("Getting information for stream...");
+	pool.query('SELECT s.name as stream_name, si.name as site_name, si.description, si.latitude, si.longitude, j.name, j.date, j.content FROM stream s INNER JOIN site si ON si.stream_id = s.id INNER JOIN journal j ON j.site_id = si.id WHERE s.name = $1::text', [stream], function(err, res) {
+		if (err) {
+			throw err;
+		}else {
+			console.log("Back from Db....");
+
+			console.log("Rows: " + res.rows);
+			var results = {
+				status : 'success',
+				list : res.rows
+			};
+
+			callback(null, results);
+
+		}
+	})
+}
+
+function getAllStreamsFromDB(callback) {
+	console.log("inside all streams...");
+	pool.query('Select name FROM stream', function(err, res) {
+		if (err) {
+			throw err; 
+		}else {
+			console.log("Back from DB with: " + res.rows);
+
+			var results = {
+				status: 'success', 
+				list: res.rows
+			};
+			callback(null, results);
+		}
+	})
+}
 
 function getStreamFromDB(stream, callback) {
 	console.log("here");
+
 	pool.query('SELECT * FROM stream WHERE name = $1::text', [stream], function(err, res) {
 		if (err) {
 			throw err;
--- public/stream.js
+++ public/stream.js
@@ -6,7 +6,7 @@ function streamSearch() {
 
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-          console.log(xmlhttp.status);
+          
            if (xmlhttp.status == 200) {
                //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
                //console.log("Back from the server with: " + xmlhttp.responseText);
@@ -21,9 +21,11 @@ function streamSearch() {
         }
     };
 
-    console.log("here at streamName");
+    
     // TODO: I need to get the book name from the form here...
-    var streamName = document.getElementById("stream").value;
+    var selectStrm = document.getElementById("selectStream");
+    var streamName = selectStrm.options[selectStrm.selectedIndex].text;
+    //console.log("here is the stream name: " + streamName);
 
     xmlhttp.open("GET", "/stream?stream=" + streamName, true);
     xmlhttp.send();	
@@ -32,11 +34,88 @@ function streamSearch() {
 function updatePage(results) {
   console.log("Updating the div with these results: " + results);
   var result = JSON.parse(results);
+  var div = document.getElementById("results"); 
+  div.innerHTML = result["list"][0]["name"] + "<button type='button' class='details btn btn-primary btn-xs detailsbtn' id='" + result["list"][0]["id"] +  "' data-toggle='modal' data-target='#myModal'>Details</button>"; //"<button data-toggle='popover' class='details btn btn-primary btn-xs detailsbtn' id='" + result["list"][0]["id"] + "' onclick()=\"showDetails()\">Details</button>";
 
+  //console.log(result["list"][0]["name"]);
 
-  console.log(result["list"][0]["name"]);
+}
 
-  //var div = getElementById("results");
-  //div.innerHTML = result["lists"];
+$(document).on('click', ".details", function(){
+    //alert($(this).attr('id'));
+    var selectStrm = document.getElementById("selectStream");
+    var streamName = selectStrm.options[selectStrm.selectedIndex].text;
+    $.ajax({type: 'GET', 
+            url: "/infoOfStream?stream=" + streamName,
+            dataType: 'json',
+            success: function(result){
+              console.log(typeof result);
+              var resultStr = "";
+              var arrayObj = result["list"];
+              for (i in arrayObj){
+                console.log(result["list"][i]);
+                resultStr += "<p>";
+                resultStr += arrayObj[i]["site_name"] + " ";
+                resultStr += arrayObj[i]["description"] + " ";
+                resultStr += arrayObj[i]["latitude"] + " " 
+                resultStr += arrayObj[i]["longitude"] + "<br/>";
+                //resultStr += arrayObj[i]["name"] + " ";
+                var date = new Date(arrayObj[i]["date"]);
+                resultStr += date.toDateString() + " ";
+                resultStr += arrayObj[i]["content"] + " ";
+                resultStr += "</p>"
 
+              }
+              $("#ele").html(resultStr);
+      
+    }});
+
+    console.log("Finsihed loading modal")
+});
+
+function loadData() {
+  console.log("Loading...");
+
+  var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+         // console.log(xmlhttp.status);
+           if (xmlhttp.status == 200) {
+               //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+               //console.log("Back from the server with: " + xmlhttp.responseText);
+               updateSelect(xmlhttp.responseText);
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+
+ 
+    xmlhttp.open("GET", "/allStreams", true);
+    xmlhttp.send(); 
+    console.log("Finished Loading");
+}
+
+function updateSelect(results) {
+
+   var result = JSON.parse(results);
+   //console.log(result);
+   var selectStreamTag = document.getElementById('selectStream');
+   index = 0;
+   for(i in result["list"]){
+    var opt = document.createElement("option");
+    opt.value= index;
+    opt.innerHTML = result["list"][i]["name"];
+
+    // next append to select tag
+    selectStreamTag.appendChild(opt);
+    index++;
+    //console.log(result["list"][i]["name"]);
+   }
+    //console.log(result["list"][0]["name"]);
 }
